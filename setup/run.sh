7647d277ed704a0f3c301c9e9c39594bcffc3f5b
Prageeth Silva <prageethsilva@gmail.com>
2020-01-11 21:04:48 +0100

    fix typo

--- setup/run.sh
+++ setup/run.sh
@@ -6,7 +6,7 @@ SETUP_SCRIPTS_DIR=$MAGIC_MIRROR_RASP_LITE_DIR/setup/scripts
 . $SETUP_SCRIPTS_DIR/install-minimal-gui.sh
 . $SETUP_SCRIPTS_DIR/install-magic-mirror.sh
 
-. $SETUP_SCRIPTS_DIR/copy-app-files-.sh
+. $SETUP_SCRIPTS_DIR/copy-app-files.sh
 . $SETUP_SCRIPTS_DIR/setup-boot-options.sh
 . $SETUP_SCRIPTS_DIR/setup-magic-mirror-service.sh
 . $SETUP_SCRIPTS_DIR/setup-plymouth.sh
