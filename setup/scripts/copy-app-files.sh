a4a6ed0dfb2482c6d43309ff213c5ba135913194
Prageeth Silva <prageethsilva@gmail.com>
2020-01-11 03:23:59 +0100

    fix problem with app directory

--- setup/scripts/copy-app-files.sh
+++ setup/scripts/copy-app-files.sh
@@ -2,8 +2,8 @@
 
 TEMPLATE_DIR=$MAGIC_MIRROR_RASP_LITE_DIR/templates
 
-info 'Copying app files...'
+info "Copying app files '$MAGIC_MIRROR_APP_DIR'"
 cp $TEMPLATE_DIR/app/* $MAGIC_MIRROR_APP_DIR
 
 # make files executable
-sudo chmod a+x -R $MAGIC_MIRROR_APP_DIR
+sudo chmod a+x -R "$MAGIC_MIRROR_APP_DIR"
